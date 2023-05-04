e0b6ca869355e05f1753b84eb38dc2ba1ba83a69
drozdovskiy.developer@gmail.com <drozdovskiy.developer@gmail.com>
2023-05-04 13:59:04 +0300

    Initial commit

---
+++ MapEditor/Controls/Menu.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace MapEditor.Controls
+{
+    public partial class Menu : UserControl
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MapEditor/Windows/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace MapEditor
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
